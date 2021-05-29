03433f4d1f8a390a88261fd2920773c82fa5ea8d
Mario Kukutsov <kukucov@gmail.com>
2021-05-30 00:24:40 +0300

    wrk on 3hw

--- M3HW/setup_swarm_dh1.sh
+++ M3HW/setup_swarm_dh1.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+docker context use alpine_remote_dh1
 docker swarm init --advertise-addr 10.1.1.2
+
+DOCKER_SWARM_TOKEN_MANAGER=$(docker swarm join-token manager --quiet)
+DOCKER_SWARM_TOKEN_WORKER=$(docker swarm join-token worker --quiet)
+
+docker context use alpine_remote_dh2
+docker swarm join --token $DOCKER_SWARM_TOKEN_MANAGER 10.1.1.2:2376 --advertise-addr 10.1.1.3
