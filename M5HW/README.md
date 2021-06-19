05b16aa72e64acd92d3c714957b8593b09e0ca7b
Mario Kukutsov <kukucov@gmail.com>
2021-06-19 23:36:55 +0300

    Removed blank become for all ansible operations. Retest / refactor of M5HW playbooks.

--- M5HW/README.md
+++ M5HW/README.md
@@ -1,16 +1,15 @@
-./deploy.sh
-    vagrant up
-        provision.sh
-        provision_ansible.sh
+As per usual 
 
-Playbooks run:
-    ansible-playbook /sync/base_playbook.yml
-    ansible-playbook /sync/servers_playbook.yml
-    ansible-playbook /sync/jenkins_master_playbook.yml
+./deploy.sh => vagrant up => provision.sh => provision_ansible.sh
+
+Playbooks run from provision_ansible.sh:
+    ...
 
 Manual steps to setup jenkins:
 http://localhost:8080
+    Unlock jenkins with password from sync/initialAdminPassword
     admin / vagrant
     Install Sugested Plugins
 
+I dont know if I need to do any manual steps but...
 
--- M5HW/provision_ansible.sh
+++ M5HW/provision_ansible.sh
@@ -6,10 +6,6 @@ sudo apt-get --assume-yes install ansible sshpass
 
 echo "Write ansible config"
 
-if [[ -f ansible.cfg ]]; then
-    rm ansible.cfg
-fi
-
 # For debuging add the two lines to ansible.cfg
 # debug = True
 # enable_task_debugger = True
@@ -21,7 +17,7 @@ host_key_checking = False
 inventory = /sync/inventory/
 EOF
 
-echo "Base Playbook"
+echo "Ansible Playbooks"
 
 # Smaller playbooks hang less often
 ansible-playbook /sync/base_playbook.yml
@@ -31,5 +27,5 @@ ansible-playbook /sync/jenkins_slave_playbook.yml
 ansible-playbook /sync/jenkins_master_playbook.yml
 
 ansible-playbook /sync/docker_install_playbook.yml
-ansible-playbook /sync/docker_swarm_playbook.yml
+ansible-playbook /sync/docker_swarm_playbook.yml # vagrant needs to logout and login again to run docker commands
 ansible-playbook /sync/docker_join_playbook.yml
